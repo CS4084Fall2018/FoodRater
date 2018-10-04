42a4e33826cbc4ddfe119e6663c0d31cbe0b196f
David Fisher <fisherds@rose-hulman.edu>
2018-10-04 13:19:22 +0100

    TODO Final step

--- FoodRater/app/src/main/java/ie/ul/davefisher/foodrater/FoodAdapter.java
+++ FoodRater/app/src/main/java/ie/ul/davefisher/foodrater/FoodAdapter.java
@@ -8,25 +8,35 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
 
-  public void addFood() {
+  private List<Food> mFoods = new ArrayList<>();
 
+  public void addFood() {
+    // TODO
   }
 
   @NonNull
   @Override
   public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    // TODO
+
     return null;
   }
 
   @Override
   public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
-
+    final Food food = mFoods.get(position);
+    holder.mName.setText(food.getName());
+    // TODO
   }
 
   @Override
   public int getItemCount() {
+    // TODO
     return 0;
   }
 
@@ -39,6 +49,21 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder
       mImageView = itemView.findViewById(R.id.food_pic);
       mName = itemView.findViewById(R.id.name);
       mRatingBar = itemView.findViewById(R.id.rating_bar);
+
+      // TODO: Delete this food on long press.
+
+      // Done together, update the rating for this food.
+      mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+          if (fromUser) {
+            // Update this food's rating
+            Food currentFood = mFoods.get(getAdapterPosition());
+            currentFood.setRating(rating);
+          }
+        }
+      });
+
     }
   }
 }
